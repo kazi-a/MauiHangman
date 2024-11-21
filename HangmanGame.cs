7053f08bb48fdc9aa52adf90c3933cfb426d41af
kazi-a <kazi.arafat@hotmail.com>
2024-11-21 17:05:03 -0500

    Add project files.

---
+++ HangmanGame.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiHangman
+{
+    public class HangmanGame
+    {
+        private readonly string _wordToGuess;
+        private readonly HashSet<char> _guessedLetters = new();
+        private const int MaxAttempts = 6;
+
+        public string CurrentGuess { get; private set; }
+        public int AttemptsLeft { get; private set; }
+        public int IncorrectGuesses { get; private set; } 
+
+        // Property to access the word 
+        public string WordToGuess => _wordToGuess;
+
+        // Constructor
+        public HangmanGame(string wordToGuess)
+        {
+            _wordToGuess = wordToGuess.ToLower();
+            AttemptsLeft = MaxAttempts;
+            IncorrectGuesses = 0; // Start with 0 incorrect guesses
+            UpdateCurrentGuess();
+        }
+
+        // Method to guess a letter
+        public bool GuessLetter(char letter)
+        {
+            letter = char.ToLower(letter);
+            if (_guessedLetters.Contains(letter)) return false; 
+
+            _guessedLetters.Add(letter);
+
+            if (_wordToGuess.Contains(letter))
+            {
+                UpdateCurrentGuess();
+                return true;
+            }
+            else
+            {
+                AttemptsLeft--;
+                IncorrectGuesses++; 
+                return false;
+            }
+        }
+
+        // Updates the current guess 
+        private void UpdateCurrentGuess()
+        {
+            CurrentGuess = new string(_wordToGuess.Select(c => _guessedLetters.Contains(c) ? c : '_').ToArray());
+        }
+
+        public string GuessedLetters => string.Join(", ", _guessedLetters.OrderBy(c => c));
+
+        public bool GameOver => AttemptsLeft <= 0 || CurrentGuess == _wordToGuess;
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+
+namespace MauiHangman
+{
+    public partial class MainPage : ContentPage
+    {
+        private HangmanGame _game;
+
+        // Random number generator
+        private Random _random = new Random();
+
+        // List of words 
+        private List<string> _words = new List<string>
+        {
+            "maui", "tropical", "volcano", "island", "adventure",
+            "hangman", "ocean", "paradise", "beach", "sunset"
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            // Select a random word from the list
+            string randomWord = _words[_random.Next(_words.Count)];
+
+            // Initialize a new Hangman game with the selected word
+            _game = new HangmanGame(randomWord);
+
+            PlayAgainButton.IsVisible = false;
+            GameOverMessage.IsVisible = false;
+            EnableLetterButtons();
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            WordDisplay.Text = string.Join(" ", _game.CurrentGuess.ToUpper().ToCharArray());
+            AttemptsLabel.Text = $"Attempts left: {_game.AttemptsLeft}";
+            GuessedLettersLabel.Text = $"Used: {_game.GuessedLetters.ToUpper()}";
+
+            // Update Hangman image 
+            UpdateHangmanImage(_game.IncorrectGuesses);
+
+            if (_game.GameOver)
+            {
+                GameOverMessage.Text = _game.CurrentGuess == _game.WordToGuess
+                    ? "You won!"
+                    : $"Game Over! The word was {_game.WordToGuess.ToUpper()}";
+                GameOverMessage.IsVisible = true;
+                PlayAgainButton.IsVisible = true;
+                DisableLetterButtons();
+            }
+        }
+
+        private void UpdateHangmanImage(int incorrectGuesses)
+        {
+            string imageName = $"hangman{incorrectGuesses}.gif";
+            HangmanImage.Source = ImageSource.FromFile(imageName);
+        }
+
+        private void DisableLetterButtons()
+        {
+            foreach (var view in LetterGrid.Children)
+            {
+                if (view is Button button)
+                {
+                    button.IsEnabled = false;
+                }
+            }
+        }
+
+        private void EnableLetterButtons()
+        {
+            foreach (var view in LetterGrid.Children)
+            {
+                if (view is Button button)
+                {
+                    button.IsEnabled = true;
+                }
+            }
+        }
+
+        private void LetterButton_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                char guessedLetter = button.Text[0];
+                _game.GuessLetter(guessedLetter);
+                button.IsEnabled = false;
+                UpdateUI();
+            }
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
